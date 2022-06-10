347d805211d0082d76cc8bd94019a93c78c4f10e
Ваше Имя <you@example.com>
2022-06-10 12:50:01 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
 import Sort from "./components/Sort";
+import Footer from "./components/Footer";
 import "./scss/app.scss";
 
-import pizzas from "./db/pizzas.json";
-
 const App = () => {
+  const [pizzas, setPizza] = React.useState([]);
   const [category, setCategory] = React.useState(0);
+
+  React.useEffect(() => {
+    console.log(123);
+    fetch("https://62a30a6421232ff9b2169b1b.mockapi.io/items")
+      .then((response) => response.json())
+      .then((json) => setPizza(json));
+  }, []);
+
   return (
     <div>
       <div className="wrapper">
@@ -21,7 +29,7 @@ const App = () => {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {category == 0
+              {category === 0
                 ? pizzas.map((item, index) => {
                     return <PizzaBlock key={item.id} item={item} />;
                   })
@@ -35,25 +43,7 @@ const App = () => {
             </div>
           </div>
         </main>
-        <footer className="footer">
-          <div className="footer__icon">
-            <a href="https://tick-time.ru/pictures/Google.png">
-              <img src="https://tick-time.ru/pictures/Google.png" alt="" />
-            </a>
-            <a href="https://tick-time.ru/pictures/App-Store.png">
-              <img src="https://tick-time.ru/pictures/App-Store.png" alt="" />
-            </a>
-          </div>
-          <p>
-            Добро пожаловать на сайт пиццерии Тик Тайм! Мы готовим и доставляем Пиццу с 2012 года!
-            Мы готовим, быстро доставляем, дарим и едим любимую пиццу! Пиццерия Тик Тайм работает
-            как небольшое заведение с доставкой по Вашему городу! Каждая Пицца готовится под заказ,
-            и это занимает всего 10 минут!
-          </p>
-          <div className="github">
-            <a href="https://github.com/GlebGlushchenko">github.com</a>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer__icon">
+        <a href="https://tick-time.ru/pictures/Google.png">
+          <img src="https://tick-time.ru/pictures/Google.png" alt="" />
+        </a>
+        <a href="https://tick-time.ru/pictures/App-Store.png">
+          <img src="https://tick-time.ru/pictures/App-Store.png" alt="" />
+        </a>
+      </div>
+      <p>
+        Добро пожаловать на сайт пиццерии Тик Тайм! Мы готовим и доставляем Пиццу с 2012 года! Мы
+        готовим, быстро доставляем, дарим и едим любимую пиццу! Пиццерия Тик Тайм работает как
+        небольшое заведение с доставкой по Вашему городу! Каждая Пицца готовится под заказ, и это
+        занимает всего 10 минут!
+      </p>
+      <div className="github">
+        <a href="https://github.com/GlebGlushchenko">github.com</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
