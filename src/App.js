5a060d6145d18cace1922acfd54cf0ee2e1b7165
Ваше Имя <you@example.com>
2022-06-15 12:33:53 +0300

    add modal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./components/pages/Home";
 import Cart from "./components/pages/Cart";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./components/pages/NotFound";
+import Modal from "./components/ModalWindow";
 
 export const SearchContex = React.createContext();
 
@@ -13,22 +14,24 @@ const App = () => {
   const [searchValue, setSearchValue] = React.useState("");
 
   return (
-    <div className="wrapper">
-      <SearchContex.Provider value={{ searchValue, setSearchValue }}>
-        <Header />
-
-        <main className="content">
-          <div className="container">
-            <Routes>
-              <Route path="/" element={<Home searchValue={searchValue} />} />
-              <Route path="/cart" element={<Cart />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-        </main>
-        <Footer />
-      </SearchContex.Provider>
-    </div>
+    <>
+      {/* <Modal /> */}
+      <div className={"wrapper"}>
+        <SearchContex.Provider value={{ searchValue, setSearchValue }}>
+          <Header />
+          <main className="content">
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home searchValue={searchValue} />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </div>
+          </main>
+          <Footer />
+        </SearchContex.Provider>
+      </div>
+    </>
   );
 };
 
